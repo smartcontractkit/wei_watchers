951b70699133d648015a2f167eb5161cd7466b72
Steve Ellis <email@steveell.is>
2016-05-17 14:58:23 -0400

    Account#notify_subscribers

--- app/models/account.rb
+++ app/models/account.rb
@@ -6,4 +6,10 @@ class Account < ActiveRecord::Base
   validates :address, format: /\A0x[0-9a-f]{40}\z/
   validates :balance, numericality: { greater_than_or_equal_to: 0, only_integer: true }
 
+  def notify_subscribers(info)
+    subscribers.each do |subscriber|
+      subscriber.notify info
+    end
+  end
+
 end
--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -6,4 +6,26 @@ describe Account, type: :model do
     it { is_expected.to have_valid(:balance).when(0, 1, 32452345234562352462346246234624626535626432623462462) }
     it { is_expected.not_to have_valid(:balance).when(nil, -1, 0.1) }
   end
+
+  describe "#notify_subscribers" do
+    let(:params) { {foo: SecureRandom.hex} }
+    let(:account) { factory_create :account }
+    let(:subscriber1) { factory_create :subscriber }
+    let(:subscriber2) { factory_create :subscriber }
+    let!(:subscriber3) { factory_create :subscriber }
+
+    before do
+      account.subscribers = [subscriber1, subscriber2]
+    end
+
+    it "notifies only it's accounts subscribers" do
+      expect(subscriber1).to receive(:notify)
+        .with(params)
+      expect(subscriber2).to receive(:notify)
+        .with(params)
+      expect(subscriber3).not_to receive(:notify)
+
+      account.notify_subscribers(params)
+    end
+  end
 end
