82aafa1f9cc625b1c4187b99db3674f65d68d72f
Steve Ellis <email@steveell.is>
2016-05-17 17:08:42 -0400

    Enforce account uniqueness

--- app/models/account.rb
+++ app/models/account.rb
@@ -5,7 +5,7 @@ class Account < ActiveRecord::Base
   has_many :subscribers, through: :subscriptions
   has_many :subscriptions, inverse_of: :account
 
-  validates :address, format: /\A0x[0-9a-f]{40}\z/
+  validates :address, format: /\A0x[0-9a-f]{40}\z/, uniqueness: true
   validates :balance, numericality: { greater_than_or_equal_to: 0, only_integer: true }
 
   def notify_subscribers(info)
