230ebb42868d5445ecf0d05bbe4b841c0805a37f
Steve Ellis <email@steveell.is>
2016-05-17 12:12:19 -0400

    clilent to handle subscriber comunication

---
+++ app/models/subscriber_client.rb
@@ -0,0 +1,31 @@
+class SubscriberClient
+
+  def self.notify(subscriber_id, params)
+    subscriber = Subscriber.find(subscriber_id)
+    new(subscriber).notify(params)
+  end
+
+  def initialize(subscriber)
+    @subscriber = subscriber
+  end
+
+  def notify(body)
+    json = JSON.parse(post(body))
+
+    if json['acknowledged_at'].blank?
+      raise "Subscriber did not acknowledge: #{json['errors']}"
+    end
+  end
+
+
+  private
+
+  attr_reader :subscriber
+
+  def post(body)
+    HTTParty.post(subscriber.notification_url, {
+      body: body
+    }).body
+  end
+
+end
---
+++ spec/models/subscriber_client_spec.rb
@@ -0,0 +1,39 @@
+describe SubscriberClient, type: :model do
+  let(:subscriber) { factory_create :subscriber }
+  let(:params) { {baz: SecureRandom.hex} }
+
+  describe ".notify" do
+    context "when the response includes an acknowledged at time" do
+      let(:response) { double body: {acknowledged_at: Time.now.to_i}.to_json }
+
+      it "posts to the subscriber's notification URL" do
+        expect(HTTParty).to receive(:post)
+          .with(subscriber.notification_url, {
+            body: params
+          })
+          .and_return(response)
+
+        expect {
+          SubscriberClient.notify(subscriber.id, params)
+        }.not_to raise_error
+      end
+    end
+
+    context "when the response does NOT include an acknowledged at time" do
+      let(:response) { double body: {errors: '["all of the errors"]'}.to_json }
+
+      it "posts to the subscriber's notification URL" do
+        expect(HTTParty).to receive(:post)
+          .with(subscriber.notification_url, {
+            body: params
+          })
+          .and_return(response)
+
+        expect {
+          SubscriberClient.notify(subscriber.id, params)
+        }.to raise_error 'Subscriber did not acknowledge: ["all of the errors"]'
+      end
+    end
+  end
+
+end
