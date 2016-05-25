946e6612ac1ae5fc5b0bbc90919308ff30c96acd
Steve Ellis <email@steveell.is>
2016-05-25 11:21:19 -0400

    Use Puma
    Also, remove gem requirements

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: bundle exec rails s -p 3174
+web: bundle exec puma -C config/puma.rb
---
+++ config/puma.rb
@@ -0,0 +1,15 @@
+workers Integer(ENV['PUMA_PROCESSES'] || 2)
+threads_count = Integer(ENV['PUMA_THREADS'] || 5)
+threads threads_count, threads_count
+
+preload_app!
+
+rackup      DefaultRackup
+port        ENV['WEI_WATCHERS_PORT'] || 3174
+environment ENV['RACK_ENV'] || 'development'
+
+on_worker_boot do
+  # Worker specific setup for Rails 4.1+
+  # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
+  ActiveRecord::Base.establish_connection
+end
