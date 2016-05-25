597fe33a313475405d5ad3e84615961c102c0b52
Steve Ellis <email@steveell.is>
2016-05-25 16:32:35 -0400

    Set up clockwork

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: bundle exec puma -C config/puma.rb
+clock:  bundle exec clockwork config/clock.rb
---
+++ config/clock.rb
@@ -0,0 +1,11 @@
+require 'clockwork'
+require './config/boot'
+require './config/environment'
+
+module Clockwork
+  handler do |job|
+    system "rails runner '#{job}'"
+  end
+
+  every(1.minute, 'BalanceCheck.schedule_checks')
+end
