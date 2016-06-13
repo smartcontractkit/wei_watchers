564ddf02963ef065bf14cd3021fa402b5a990117
Steve Ellis <email@steveell.is>
2016-06-13 11:03:27 -0400

    Better logging

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,8 @@ module WeiWatchers
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.logger = Logger.new(STDOUT)
+    config.logger.level = Logger.const_get(ENV['LOG_LEVEL'] ? ENV['LOG_LEVEL'].upcase : 'INFO')
   end
 end
