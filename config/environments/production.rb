2d7da7052c559e5188bce7d2099e7c4aa59dc307
Steve Ellis <email@steveell.is>
2016-08-26 16:19:08 -0400

    fix forcing SSL

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+
+  config.force_ssl = true
+  config.middleware.use ActionDispatch::Flash
 end
