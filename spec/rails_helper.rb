5e494261ddb8fd59b750acbd890b21fa875de3ab
Steve Ellis <email@steveell.is>
2016-12-07 17:25:41 -0500

    move spec helpers into modules

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -10,6 +10,8 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
+  include SpecHelpers
+
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
--- spec/support/general.rb
+++ spec/support/general.rb
@@ -1,22 +1,26 @@
-def hashie(hash)
-  Hashie::Mash.new hash
-end
+module SpecHelpers
 
-def factory_create(factory_name, options = {})
-  FactoryGirl.create(factory_name, options)
-end
+  def hashie(hash)
+    Hashie::Mash.new hash
+  end
 
-def factory_build(factory_name, options = {})
-  FactoryGirl.build(factory_name, options)
-end
+  def factory_create(factory_name, options = {})
+    FactoryGirl.create(factory_name, options)
+  end
 
-def basic_auth_login(subscriber = factory_create(:subscriber))
-  user = subscriber.api_id
-  password = subscriber.api_key
-  auth = ActionController::HttpAuthentication::Basic.encode_credentials user, password
-  request.env['HTTP_AUTHORIZATION'] = auth
-end
+  def factory_build(factory_name, options = {})
+    FactoryGirl.build(factory_name, options)
+  end
 
-def json_response
-  JSON.parse(response.body)
+  def basic_auth_login(subscriber = factory_create(:subscriber))
+    user = subscriber.api_id
+    password = subscriber.api_key
+    auth = ActionController::HttpAuthentication::Basic.encode_credentials user, password
+    request.env['HTTP_AUTHORIZATION'] = auth
+  end
+
+  def json_response
+    JSON.parse(response.body)
+  end
+
 end
