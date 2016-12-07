2574d297d1034f5e9d711f8a3e21cc6eba9748de
Steve Ellis <email@steveell.is>
2016-12-07 17:42:04 -0500

    set up dev network to run with test suite

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -16,4 +16,32 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
+
+  config.add_setting :geth_pid
+
+  config.before(:suite) do |example|
+    port = 7434
+    geth = "geth --dev --mine --fakepow --etherbase 0x4f5f1e8029640b873a43c427e9a4f03cf56ec6d8 --rpc --rpccorsdomain '*' --rpcport #{port} --ipcpath './tmp/geth.ipc' --datadir './tmp' --lightkdf --verbosity 0"
+    closed = true
+    attempts = 0
+
+    geth_pid = fork { exec geth }
+    RSpec.configuration.geth_pid = geth_pid
+    Process.detach(geth_pid)
+
+    while (closed && attempts < 60) do
+      if port_open?('localhost', port, 30)
+        closed = false
+      else
+        attempts += 1
+        sleep 0.5
+      end
+    end
+
+    raise "geth didn't start after 30 seconds" if closed
+  end
+
+  config.after(:suite) do |example|
+    Process.kill "TERM", RSpec.configuration.geth_pid
+  end
 end
--- spec/support/general.rb
+++ spec/support/general.rb
@@ -23,4 +23,17 @@ module SpecHelpers
     JSON.parse(response.body)
   end
 
+  def port_open?(ip, port, seconds=1)
+    Timeout::timeout(seconds) do
+        begin
+          TCPSocket.new(ip, port).close
+          true
+        rescue Errno::ECONNREFUSED, Errno::EHOSTUNREACH
+          false
+        end
+      end
+    rescue Timeout::Error
+      false
+  end
+
 end
