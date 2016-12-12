fce12f188e78dda7ed58cf6d7a81f9cb06626559
Steve Ellis <email@steveell.is>
2016-12-12 18:07:15 -0500

    ensure unique event topics

--- app/models/event_topic.rb
+++ app/models/event_topic.rb
@@ -3,6 +3,6 @@ class EventTopic < ActiveRecord::Base
   has_many :log_event_topics, inverse_of: :event_topic
   has_many :log_events, through: :log_event_topics
 
-  validates :topic, format: /\A0x[0-9a-f]{64}\z/
+  validates :topic, format: /\A0x[0-9a-f]{64}\z/, uniqueness: true
 
 end
--- spec/models/event_topic_spec.rb
+++ spec/models/event_topic_spec.rb
@@ -1,8 +1,10 @@
 describe EventTopic, type: :model do
 
   describe "validations" do
+    let(:old_topic) { factory_create(:event_topic).topic }
+
     it { is_expected.to have_valid(:topic).when("0x#{SecureRandom.hex 32}") }
-    it { is_expected.not_to have_valid(:topic).when(nil, '', '0x' "0x#{SecureRandom.hex 33}") }
+    it { is_expected.not_to have_valid(:topic).when(nil, '', '0x' "0x#{SecureRandom.hex 33}", old_topic) }
   end
 
 end
