0f649c7b3cdf369b567e2e74e80cc649658a9f39
Steve Ellis <email@steveell.is>
2016-12-13 16:13:24 -0500

    make event topics encompass more values

--- app/models/event_topic.rb
+++ app/models/event_topic.rb
@@ -3,6 +3,6 @@ class EventTopic < ActiveRecord::Base
   has_many :event_log_topics, inverse_of: :event_topic
   has_many :event_logs, through: :event_log_topics
 
-  validates :topic, format: /\A0x[0-9a-f]{64}\z/, uniqueness: true
+  validates :topic, format: /\A0x[0-9a-f]*\z/, uniqueness: true
 
 end
