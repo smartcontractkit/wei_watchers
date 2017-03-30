16eb1a59cdff7ba010f810731a38b2e1c8859a40
Steve Ellis <email@steveell.is>
2017-03-30 15:50:04 -0700

    remove geth requirement from testing

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -17,34 +17,6 @@ RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
 
-  config.add_setting :geth_pid
-
-  config.before(:suite) do |example|
-    port = 7434
-    geth = "geth --dev --mine --fakepow --etherbase #{default_address} --rpc --rpccorsdomain '*' --rpcport #{port} --ipcpath './tmp/geth.ipc' --datadir './tmp' --lightkdf --verbosity 0"
-    closed = true
-    attempts = 0
-
-    geth_pid = fork { exec geth }
-    RSpec.configuration.geth_pid = geth_pid
-    Process.detach(geth_pid)
-
-    while (closed && attempts < 60) do
-      if port_open?('localhost', port, 30)
-        closed = false
-      else
-        attempts += 1
-        sleep 0.5
-      end
-    end
-
-    raise "geth didn't start after 30 seconds" if closed
-  end
-
-  config.after(:suite) do |example|
-    Process.kill "TERM", RSpec.configuration.geth_pid
-  end
-
   config.before do
     allow(EthereumClient).to receive(:post)
       .and_return(http_response body: {result: ethereum_txid}.to_json)
