e07fc9e3a4889cf65b27c74458e023fd956b612b
Steve Ellis <email@steveell.is>
2017-04-25 20:16:54 -0700

    track subscription last checked

--- app/models/filter_check.rb
+++ app/models/filter_check.rb
@@ -2,35 +2,46 @@ class FilterCheck
 
   include HasEthereumClient
 
-  def self.schedule_checks
+  def self.schedule_checks(current_block = nil)
+    current_block ||= EthereumClient.new.current_block_height
+
     EventSubscription.current.pluck(:id).uniq.each do |subscription_id|
-      delay.perform subscription_id
+      delay.perform subscription_id, current_block
     end
   end
 
-  def self.perform(id)
+  def self.perform(id, current_block = nil)
     subscription = EventSubscription.find(id)
-    new(subscription).perform
+    new(subscription, current_block).perform
   end
 
-  def initialize(subscription)
+  def initialize(subscription, current_block = nil)
     @subscription = subscription
     @filter = subscription.filter
+    @current_block = current_block
   end
 
   def perform
     new_logs.each do |log|
       EventLogger.delay.perform(subscription.id, log)
     end
+
+    update_subscription_block_height
   end
 
 
   private
 
-  attr_reader :filter, :subscription
+  attr_reader :current_block, :filter, :subscription
 
   def new_logs
     @new_logs ||= ethereum.get_filter_changes(filter)
+  end
+
+  def update_subscription_block_height
+    if current_block.present?
+      subscription.update_attributes! last_block_height: current_block
+    end
   end
 
 end
--- spec/models/filter_check_spec.rb
+++ spec/models/filter_check_spec.rb
@@ -3,6 +3,7 @@ describe FilterCheck, type: :model do
   describe ".perform" do
     let(:subscription) { factory_create :event_subscription }
     let(:raw_event) { double :raw_event }
+    let(:block_height) { 1_234_567 }
 
     it "logs whichever events it gets back from ethereum" do
       expect_any_instance_of(EthereumClient).to receive(:get_filter_changes)
@@ -13,6 +14,14 @@ describe FilterCheck, type: :model do
         .with(subscription.id, raw_event)
 
       FilterCheck.perform subscription.id
+    end
+
+    it "updates the subscription's last block height" do
+      expect {
+        FilterCheck.perform subscription.id, block_height
+      }.to change {
+        subscription.reload.last_block_height
+      }.to(block_height)
     end
   end
 
