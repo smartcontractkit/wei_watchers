417fd21333b0912de63a92b9973d423fef084269
Steve Ellis <email@steveell.is>
2017-05-09 19:19:33 -0700

    allow for different cased addresses

--- app/models/account.rb
+++ app/models/account.rb
@@ -5,7 +5,7 @@ class Account < ActiveRecord::Base
   has_many :balance_subscriptions, inverse_of: :account
   has_many :subscribers, through: :balance_subscriptions
 
-  validates :address, format: /\A0x[0-9a-f]{40}\z/, uniqueness: true
+  validates :address, format: /\A0x[0-9a-f]{40}\z/i, uniqueness: true
   validates :balance, numericality: { greater_than_or_equal_to: 0, only_integer: true }
 
   def notify_subscribers(info)
--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -2,7 +2,7 @@ describe Account, type: :model do
   describe "validations" do
     let!(:old_account) { factory_create :account }
 
-    it { is_expected.to have_valid(:address).when("0x#{SecureRandom.hex 20}") }
+    it { is_expected.to have_valid(:address).when("0x#{SecureRandom.hex(20).upcase}") }
     it { is_expected.not_to have_valid(:address).when(nil, '', old_account.address, "0x", SecureRandom.hex(20), "0x#{SecureRandom.hex 19}") }
 
     it { is_expected.to have_valid(:balance).when(0, 1, 32452345234562352462346246234624626535626432623462462) }
